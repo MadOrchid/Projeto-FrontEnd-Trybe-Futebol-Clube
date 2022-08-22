f52c7880d7b65d471bf1e45746d531f76eb20005
Leonardo <leo.agb_@hotmail.com>
2022-08-22 00:42:37 -0300

    models e migration users

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+export default class User extends Model {
+  public username: string;
+  public role: string;
+  public email: string;
+  public passowrd: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'User',
+  timestamps: false,
+});
