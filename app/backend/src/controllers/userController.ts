a2af6a8470fbadd08c035cd638cbece53c05da04
Leonardo <leo.agb_@hotmail.com>
2022-08-23 16:05:15 -0300

    Message bad request

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -10,6 +10,10 @@ export default class UserController {
 
   public login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
     const token = await this.service.login(email, password);
     res.status(200).json({ token });
   };
