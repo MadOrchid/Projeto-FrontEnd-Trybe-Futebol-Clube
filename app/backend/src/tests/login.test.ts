4049edd16ae2f3fc297f72e88f3788e2dc1c4e96
Leonardo <leo.agb_@hotmail.com>
2022-08-23 15:55:21 -0300

    teste loguin

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,32 @@
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import User from '../database/models/user';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const mock: any = {
+  id: 1,
+  username: 'test',
+  role: 'any',
+  email: 'test@test.com',
+  password: '123456',
+}
+
+describe('login', () => {
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  it('login sucess', async () => {
+    sinon.stub(User, 'findOne').resolves(mock);
+
+    const response = await chai.request(app).post('/login');
+    expect(response.body).to.have.key('token');
+  })
+})
