101e424b242a37c7edc6f4ab2740f6ea737f676a
Leonardo <leo.agb_@hotmail.com>
2022-08-28 23:26:01 -0300

    fix:validation

--- app/backend/src/controllers/userController.ts
+++ app/backend/src/controllers/userController.ts
@@ -8,13 +8,20 @@ export default class UserController {
     this.service = new UserService();
   }
 
-  public login = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  public login = async (request: Request, response: Response) => {
+    const { email, password } = request.body;
 
     if (!email || !password) {
-      return res.status(400).json({ message: 'All fields must be filled' });
+      return response.status(400).json({ message: 'All fields must be filled' });
     }
+
     const token = await this.service.login(email, password);
-    res.status(200).json({ token });
+    response.status(200).json({ token });
+  };
+
+  public validate = async (request: Request, response: Response) => {
+    const { authorization } = request.headers;
+    const user = await this.service.validate(authorization as string);
+    response.status(200).json({ role: user?.role });
   };
 }
--- app/backend/src/services/userService.ts
+++ app/backend/src/services/userService.ts
@@ -1,17 +1,37 @@
+import { compare } from 'bcryptjs';
 import 'dotenv/config';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import User from '../database/models/user';
 
-export default class UserService {
-  public getEmail = async (email: string): Promise<User | null> => {
-    const user: User | null = await User.findOne({
-      where: { email },
-    });
-    return user;
-  };
+interface IToken {
+  email: string
+}
 
-  public login = async (email: string, password:string): Promise<string> => {
+export default class Userervice {
+  public login = async (email: string, password: string) => {
+    /* const user = await User.findOne({ where: { email } });
+    if (!user) {
+      throw new Error('Incorrect email or password');
+    }
+
+    const comparePassword = await compare(password, user.password);
+    if (!comparePassword) {
+      throw new Error('Incorrect email or password');
+    } */
+
     const token = sign({ email, password }, process.env.JWT_SECRET || 'jwt_secret');
     return token;
+  };
+
+  public validate = async (authorization: string) => {
+    try {
+      const secret = process.env.JWT_SECRET as string;
+      const token = verify(authorization, secret);
+      const { email } = token as IToken;
+      const user = await User.findOne({ where: { email } });
+      return user;
+    } catch (error) {
+      throw new Error('Token must be a valid token');
+    }
   };
 }
