795aa98ed9f8794aaf7f2062474b4ad2f3705c13
Leonardo <leo.agb_@hotmail.com>
2022-08-30 19:59:36 -0300

    messagem error reqs 26, 27

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import ErrorHandler from '../middleware/middleware';
 import MatchesServicece from '../services/matchService';
 import TeamService from '../services/teamService';
 
@@ -12,8 +13,7 @@ export default class MatchesController {
     const { homeTeam, awayTeam } = req.body;
 
     if (homeTeam === awayTeam) {
-      res.status(401)
-        .json({ message: 'It is not possible to create a match with two equal teams' });
+      throw new ErrorHandler(401, 'It is not possible to create a match with two equal teams');
     }
     const match = await MatchesServicece.newMatch(req.body);
     res.status(201).json(match);
@@ -21,7 +21,7 @@ export default class MatchesController {
     const team = await TeamService.getById(homeTeam);
 
     if (!team) {
-      res.status(404).json({ message: 'There is no team with such id' });
+      throw new ErrorHandler(404, 'There is no team with such id');
     }
   }
 
