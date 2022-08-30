1c216a18b738d397cf8d0ea6396b6634f87811c6
Leonardo <leo.agb_@hotmail.com>
2022-08-30 20:34:42 -0300

    volta ultimo commit

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import ErrorHandler from '../middleware/middleware';
 import MatchesServicece from '../services/matchService';
 import TeamService from '../services/teamService';
 
@@ -13,7 +12,8 @@ export default class MatchesController {
     const { homeTeam, awayTeam } = req.body;
 
     if (homeTeam === awayTeam) {
-      throw new ErrorHandler(401, 'It is not possible to create a match with two equal teams');
+      res.status(401)
+        .json({ message: 'It is not possible to create a match with two equal teams' });
     }
     const match = await MatchesServicece.newMatch(req.body);
     res.status(201).json(match);
@@ -21,7 +21,7 @@ export default class MatchesController {
     const team = await TeamService.getById(homeTeam);
 
     if (!team) {
-      throw new ErrorHandler(404, 'There is no team with such id');
+      res.status(404).json({ message: 'There is no team with such id' });
     }
   }
 
